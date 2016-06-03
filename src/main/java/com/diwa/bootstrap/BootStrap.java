a019046837ca193aab8755340717df446eeefc2e
diwa <di@diwas-MacBook-Pro.local>
2016-06-03 15:28:35 +0800

    add logback

---
+++ src/main/java/com/diwa/bootstrap/BootStrap.java
@@ -0,0 +1,17 @@
+package com.diwa.bootstrap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by di on 3/6/2016.
+ */
+public class BootStrap {
+    private static final Logger logger = LoggerFactory.getLogger(BootStrap.class);
+
+    public static void main(String[] args) {
+        logger.info("BootStrap Main begin.");
+
+
+    }
+}
