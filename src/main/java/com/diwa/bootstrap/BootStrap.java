afe775699272976f8b03cb06a2cfbed97c2eb151
diwa <di@diwas-MacBook-Pro.local>
2016-06-03 15:40:38 +0800

    add dependency

--- src/main/java/com/diwa/bootstrap/BootStrap.java
+++ src/main/java/com/diwa/bootstrap/BootStrap.java
@@ -2,6 +2,8 @@ package com.diwa.bootstrap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by di on 3/6/2016.
@@ -12,6 +14,11 @@ public class BootStrap {
     public static void main(String[] args) {
         logger.info("BootStrap Main begin.");
 
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/root-context.xml");
+
+        String displayName = applicationContext.getDisplayName();
+
+        System.out.println(displayName);
 
     }
 }
